eb9f25fa7ad4eaa526799a8033f67e0a1f7924b1
karinpang <atelierkarin@gmail.com>
2020-05-06 09:58:06 +0800

    upload travis

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# Build Images
+docker build -t atelierkarin/test-server:latest -t atelierkarin/test-server:$GIT_SHA -f
+
+# Push to docker hub
+docker push atelierkarin/test-server:latest
+docker push atelierkarin/test-server:$GIT_SHA
+
+# Apply config file to k8s
+kubectl apply -f k8s
+
+# Update images
+kubectl set image deployments/test-server-deployment server=atelierkarin/test-server:$GIT_SHA
